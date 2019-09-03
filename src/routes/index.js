8a506b0b4c087fd1a80603f2fff6d0b864a1a99b
LesegoGomolemo <lesegomabe@ymail.com>
2019-09-03 22:37:42 +0200

    Example pages and their routes.

--- src/routes/index.js
+++ src/routes/index.js
@@ -6,4 +6,30 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Fire Breathing Rubber Duckies' });
 });
 
+/* GET game page. */
+router.get('/game', function(req, res, next) {
+  res.render('game', { title: 'Coolest Game Ever' });
+});
+
+/* GET project X. */
+router.get('/projectX', function(req, res, next) {
+  res.render('projectX', { title: 'Project X' });
+});
+
+/* GET horror short page. */
+router.get('/horror', function(req, res, next) {
+  res.render('horrorShort', { title: 'No Strings Attached' });
+});
+
+/* GET botic project page. */
+router.get('/botic', function(req, res, next) {
+  res.render('botic', { title: 'Botic: Privacy aware chatbot'});
+});
+
+/* GET ERP Ranger Mobile App project page. */
+router.get('/erp', function(req, res, next) {
+  res.render('erpRanger', { title: 'ERP Ranger Mobile Application' });
+});
+
+
 module.exports = router;
