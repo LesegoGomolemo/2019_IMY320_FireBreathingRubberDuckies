be8f0451c1eae7a725469ac6e5ea3e0b47ce75d1
LesegoGomolemo <lesegomabe@ymail.com>
2019-09-07 20:44:40 +0200

    Included header, added variables in routes.

--- src/routes/index.js
+++ src/routes/index.js
@@ -8,27 +8,27 @@ router.get('/', function(req, res, next) {
 
 /* GET game page. */
 router.get('/game', function(req, res, next) {
-  res.render('game', { title: 'Coolest Game Ever' });
+  res.render('game', { title: 'Coolest Game Ever' , quote: 'We could all be reading a book right now, but lets play a few more rounds.'});
 });
 
 /* GET project X. */
 router.get('/projectX', function(req, res, next) {
-  res.render('projectX', { title: 'Project X' });
+  res.render('projectX', { title: 'Project X' , quote: 'This project will be revealed after the raid on Area 51.'});
 });
 
 /* GET horror short page. */
 router.get('/horror', function(req, res, next) {
-  res.render('horrorShort', { title: 'No Strings Attached' });
+  res.render('horrorShort', { title: 'No Strings Attached' , quote: 'Horror short written and directed by the Fire Breathing Rubber Duckies'});
 });
 
 /* GET botic project page. */
 router.get('/botic', function(req, res, next) {
-  res.render('botic', { title: 'Botic: Privacy aware chatbot'});
+  res.render('botic', { title: 'Botic: Privacy aware chatbot' , quote: 'Botic Privacy Chatbot, by Alabama Liquid Snakes for EPI USE Labs'});
 });
 
 /* GET ERP Ranger Mobile App project page. */
 router.get('/erp', function(req, res, next) {
-  res.render('erpRanger', { title: 'ERP Ranger Mobile Application' });
+  res.render('erpRanger', { title: 'ERP Ranger Mobile Application' , quote: 'ERP Mobile Application project by Tenacious Technicians for EPI Use.'});
 });
 
 
