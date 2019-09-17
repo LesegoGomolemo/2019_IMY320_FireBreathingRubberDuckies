0855827e55804a66c2a3798366cbaa0835cbe4df
LesegoGomolemo <lesegomabe@ymail.com>
2019-09-17 09:50:10 +0200

    Slidey slidey; Home page content migration; Projects page migration.

--- src/fire-portfolio/src/app/app-routing.module.ts
+++ src/fire-portfolio/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './components/pages/home/home.component';
+import { ProjectsComponent } from './components/pages/projects/projects.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: HomeComponent, pathMatch: 'full', data: { animation: 'HomePage'} },
+  { path: 'project', component: ProjectsComponent, data: { animation: 'ProjectsPage'} }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/fire-portfolio/src/app/app.module.ts
+++ src/fire-portfolio/src/app/app.module.ts
@@ -1,15 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/pages/home/home.component';
+import { ProjectsComponent } from './components/pages/projects/projects.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ProjectsComponent
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule
   ],
   providers: [],
