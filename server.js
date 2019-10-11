9d8435a12e750049f6aac6fff7ac169a0b2e6e89
Unknown <justin.grenfell110@gmail.com>
2019-10-11 22:38:47 +0200

    Recaigged and fixed all the files

---
+++ server.js
@@ -0,0 +1,50 @@
+
+// Dependencies
+const express = require('express');
+const path = require('path');
+const cors = require('cors');
+
+// App
+const app = express();
+app.use(cors());
+
+// Security middleware
+
+/*
+function resSec(req, res, next) {
+    // not using SSL yet
+    /*if (app.get('env') !== 'stage') {
+        // HTTP Strict Transport Security (HSTS)
+        // Enforces HTTPS across the entire app
+        // While nginx can do a redirect, HSTS redirects
+        // before anything is sent to the server
+        // (Only run this in an SSL environment)
+        res.seHeader('Strict-Transport-Security', 'max-age=630720');
+    }*/
+
+    // Defending against Cross Site Scripting (XSS): when a malicious entity injects scripts
+//    res.setHeader('X-XSS-Protection', '1; mode=block');
+
+    // Require iFrame sources to come from the same origin
+//    res.setHeader('X-Frame-Options', 'SAMEORIGIN');
+
+    // Content Security Policy: Preventing XSS to ensure that scripts only come from approved origins
+//    res.setHeader("Content-Security-Policy", "script-src 'self'");
+
+    //send the request onwards with security headers
+//    return next();
+//}
+//app.use(resSec);
+
+
+// Set a static path to the Angular app
+app.use('/', express.static(path.join(__dirname, './dist/fire-portfolio')));
+
+// Pass routing to the Angular App
+app.get('*', function(req,res) {
+    res.sendFile(path.join(__dirname, '/dist/fire-portfolio/index.html'));
+});
+
+// Start the app by listening on the default Heroku port
+
+app.listen(process.env.PORT || 4200);
---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './components/pages/home/home.component';
+import { ProjectsComponent } from './components/pages/projects/projects.component';
+import { GameComponent } from './components/pages/game/game.component';
+import { BoticComponent } from './components/pages/botic/botic.component';
+import { AboutComponent } from './components/pages/about/about.component';
+import { ErpComponent } from './components/pages/erp/erp.component';
+import { MovieComponent } from './components/pages/movie/movie.component';
+
+const routes: Routes = [
+  { path: '', component: HomeComponent, pathMatch: 'full', data: { animation: 'HomePage'} },
+  { path: 'home', component: HomeComponent, pathMatch: 'full', data: { animation: 'HomePage'} },
+  // { path: 'project', component: ProjectsComponent, data: { animation: 'ProjectsPage'} },
+  { path: 'game', component: GameComponent, data: {animation: 'FilterPage'} },
+  { path: 'botic', component: BoticComponent, data: {animation: 'FilterPage'} },
+  { path: 'about', component: AboutComponent, data: {animation: 'FilterPage'} },
+  { path: 'erp', component: ErpComponent, data: {animation: 'FilterPage'} },
+  { path: 'movie', component: MovieComponent, data: {animation: 'FilterPage'} },
+  { path: '**', component: HomeComponent, data: { animation: 'FilterPage'} }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
